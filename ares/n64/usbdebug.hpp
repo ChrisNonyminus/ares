710d601666336c92258f4d68ccccd3e00d928514
Christian <chrisnonymous2000@gmail.com>
2023-06-01 08:52:12 -0600

    64doom test

--- ares/n64/usbdebug.hpp
+++ ares/n64/usbdebug.hpp
@@ -31,8 +31,13 @@ constexpr u32 USB_BUFFERSIZE = 512;
 #define SC64_CMD_DEBUG_RX_BUSY ('F')
 #define SC64_CMD_DEBUG_RX_DATA ('E')
 #define SC64_CMD_DEBUG_RESET ('B')
+#define SC64_CMD_USB_READ_STATUS ('u')
+#define SC64_CMD_USB_WRITE_STATUS ('U')
+#define SC64_CMD_USB_READ ('m')
+#define SC64_CMD_USB_WRITE ('M')
 
 #define SC64_CFG_ID_SDRAM_WRITABLE (2)
+#define SC64_CFG_ROM_WRITE_ENABLE 1
 
 #define SC64_ARGS(args, a0, a1)                                                \
     {                                                                          \
@@ -105,7 +110,7 @@ struct SC64
     {
         u32 addr, len, hdr, i;
         u8 *tmp;
-        static ENetHost *enetClient;
+        static ENetHost *enetHost;
         static ENetAddress enetAddress;
         static ENetPeer *enetPeer;
         static ENetEvent enetEvent;
@@ -120,57 +125,69 @@ struct SC64
 
         switch (cmd)
         {
+        case SC64_CMD_USB_WRITE_STATUS:
         case SC64_CMD_DEBUG_TX_READY:
         {
             data[0] = ((u32)txready);
             sr = 0;
             break;
         }
+        case SC64_CMD_USB_READ_STATUS:
         case SC64_CMD_DEBUG_RX_READY:
         {
-            data[0] = 0; // header
+            data[0] = htobe32(rxbusy ? (1 << 31) : 0); // header
             data[1] = 0; // ???
                          // TODO
             if (rxready)
             {
-                data[0] = rxdata[0];
-                data[1] = rxdata[1];
+                data[0] = htobe32(rxdata[0] | (rxbusy ? (1 << 31) : 0));
+                data[1] = htobe32(rxdata[1]);
                 rxready = false;
             }
+            rxdata[0] = 0;
+            rxdata[1] = 0;
             sr = 0;
             break;
         }
         case SC64_CMD_DEBUG_TX_DATA:
+        case SC64_CMD_USB_WRITE:
         {
-            addr = (data[0]);
-            len = (data[1]);
+            if (cmd == SC64_CMD_USB_WRITE) {
+
+            addr = (data[0]) - SC64_SDRAM_BASE;
+            } else addr = data[0];
+            len = USBHEADER_GETSIZE(data[1]);
             if (addr == USB_DEBUG_ADDRESS)
             {
                 txready = false;
                 addr += SC64_SDRAM_BASE;
                 // getting usb tx data
-                if (!((cartridge.rom.read<Word>(addr)) == 0x444D4140))
-                { // DMA@
-                    printf(
-                        "dma sig was incorrect (expected 'DMA@', got %08X)\n",
-                        (cartridge.rom.read<Word>(addr)));
-                    exit(1);
-                }
+                if (cmd == SC64_CMD_DEBUG_TX_DATA) {
+                    if (!((cartridge.rom.read<Word>(addr)) == 0x444D4140))
+                    { // DMA@
+                        printf(
+                            "dma sig was incorrect (expected 'DMA@', got %08X)\n",
+                            (cartridge.rom.read<Word>(addr)));
+                        exit(1);
+                    }
+                    addr += 4;
+                    hdr = (cartridge.rom.read<Word>(addr));
+                    if (USBHEADER_GETSIZE(hdr) > len)
+                    {
+                        printf("data size was bigger than expected!\n");
+                        exit(1);
+                    }
                 addr += 4;
-                hdr = (cartridge.rom.read<Word>(addr));
-                if (USBHEADER_GETSIZE(hdr) > len)
-                {
-                    printf("data size was bigger than expected!\n");
-                    exit(1);
+                } else {
+                    hdr = data[1];
                 }
-                printf("size = %08X\n", (USBHEADER_GETSIZE(hdr)));
+                //printf("size = %08X\n", (USBHEADER_GETSIZE(hdr)));
                 tmp = new u8[(USBHEADER_GETSIZE(hdr))];
-                addr += 4;
                 for (i = 0; i < (USBHEADER_GETSIZE(hdr)); i++)
                 {
                     tmp[i] = cartridge.rom.read<Byte>(addr + i);
                 }
-                if (!enetPeer)
+                if (!enetHost)
                 {
                     if (enet_initialize() != 0)
                     {
@@ -179,14 +196,79 @@ struct SC64
                     }
                 }
 
-                printf("hdr=%08X\n", hdr);
+                //printf("hdr=%08X\n", hdr);
 
                 switch (USBHEADER_GETTYPE(hdr))
                 {
+                case NETTYPE_UDP_START_SERVER: {
+                    if (enetHost)
+                        enet_host_destroy(enetHost);
+                    int port = atoi((const char *)&tmp[0]);
+                    enetAddress.host = ENET_HOST_ANY;
+                    enetAddress.port = port;
+                    enetHost = enet_host_create(&enetAddress, 3, 2, 0, 0);
+                    if (!enetHost) {
+                        printf("Error creating server.\n");
+                        sr |= SC64_CFG_SR_CMD_ERROR;
+                        delete[] tmp;
+                        exit(1);
+                    }
+                    rxready = true;
+                    // rxdata[0] = USBHEADER_CREATE(NETTYPE_UDP_START_SERVER,
+                    //                              0);
+                    // rxdata[1] = USBHEADER_CREATE(NETTYPE_UDP_START_SERVER,
+                    //                              strlen((const char *)&tmp[0]));
+                    strcpy((char *)&buffer[0], (const char *)&tmp[0]);
+                    buffer[strlen((const char *)&tmp[0])] = '\0';
+                    waitThread = std::thread(
+                        [&]
+                        {
+                            while (true)
+                            {
+                                while (enet_host_service(enetHost, &enetEvent,
+                                                         1000))
+                                {
+                                    switch (enetEvent.type)
+                                    {
+
+                                    case ENET_EVENT_TYPE_RECEIVE:
+                                    {
+                                        // printf("\nA packet of length %08X "
+                                        //        "containing %s was received "
+                                        //        "from %u on channel %u.\n",
+                                        //        enetEvent.packet->dataLength,
+                                        //        enetEvent.packet->data,
+                                        //        enetEvent.peer->connectID,
+                                        //        enetEvent.channelID);
+                                        rxready = true;
+                                        rxdata[0] = USBHEADER_CREATE(
+                                            NETTYPE_UDP_SEND,
+                                            0);
+                                        rxdata[1] =
+                                            enetEvent.packet->dataLength;
+                                        memcpy(&buffer[0],
+                                               enetEvent.packet->data,
+                                               enetEvent.packet->dataLength);
+                                        /* Clean up the packet now that we're
+                                         * done using it. */
+                                        enet_packet_destroy(enetEvent.packet);
+                                        // while (rxready)
+                                        // {
+                                        // }
+                                    }
+                                    break;
+                                    default:
+                                        break;
+                                    }
+                                }
+                            }
+                        });
+                    break;
+                }
                 case NETTYPE_UDP_CONNECT:
                 {
-                    if (enetClient)
-                        enet_host_destroy(enetClient);
+                    if (enetHost)
+                        enet_host_destroy(enetHost);
                     std::string hostname((const char *)&tmp[0]);
                     std::string host = hostname;
                     int port = 0;
@@ -199,7 +281,7 @@ struct SC64
                         port = atoi(s.c_str());
                     }
 
-                    enetClient = enet_host_create(
+                    enetHost = enet_host_create(
                         NULL /* create a client host */,
                         1 /* only allow 1 outgoing connection */,
                         2 /* allow up 2 channels to be used, 0 and 1 */,
@@ -214,10 +296,10 @@ struct SC64
                         printf("Error setting host: %08X\n", res);
                         sr |= SC64_CFG_SR_CMD_ERROR;
                         delete[] tmp;
-                        return;
+                        exit(1);
                     }
                     enetPeer =
-                        enet_host_connect(enetClient, &enetAddress, 2, 0);
+                        enet_host_connect(enetHost, &enetAddress, 2, 0);
                     if (!enetPeer)
                     {
                         sr |= SC64_CFG_SR_CMD_ERROR;
@@ -226,9 +308,10 @@ struct SC64
                     }
                     printf("Connected to host %08X\n", enetPeer->address.host);
                     rxready = true;
-                    rxdata[0] = USBHEADER_CREATE(NETTYPE_UDP_CONNECT,
-                                                 strlen(host.c_str()));
-                    rxdata[1] = strlen(host.c_str());
+                    // rxdata[0] = USBHEADER_CREATE(NETTYPE_UDP_CONNECT,
+                    //                              0);
+                    // rxdata[1] = USBHEADER_CREATE(NETTYPE_UDP_CONNECT,
+                    //                              strlen(host.c_str()));
                     strcpy((char *)&buffer[0], host.c_str());
                     buffer[strlen(host.c_str())] = '\0';
                     waitThread = std::thread(
@@ -236,7 +319,7 @@ struct SC64
                         {
                             while (true)
                             {
-                                while (enet_host_service(enetClient, &enetEvent,
+                                while (enet_host_service(enetHost, &enetEvent,
                                                          1000))
                                 {
                                     switch (enetEvent.type)
@@ -244,17 +327,17 @@ struct SC64
 
                                     case ENET_EVENT_TYPE_RECEIVE:
                                     {
-                                        printf("\nA packet of length %zu "
-                                               "containing %s was received "
-                                               "from %u on channel %u.\n",
-                                               enetEvent.packet->dataLength,
-                                               enetEvent.packet->data,
-                                               enetEvent.peer->connectID,
-                                               enetEvent.channelID);
+                                        // printf("\nA packet of length 0x%08X "
+                                        //        "containing %s was received "
+                                        //        "from %u on channel %u.\n",
+                                        //        enetEvent.packet->dataLength,
+                                        //        enetEvent.packet->data,
+                                        //        enetEvent.peer->connectID,
+                                        //        enetEvent.channelID);
                                         rxready = true;
                                         rxdata[0] = USBHEADER_CREATE(
                                             NETTYPE_UDP_SEND,
-                                            enetEvent.packet->dataLength);
+                                            0);
                                         rxdata[1] =
                                             enetEvent.packet->dataLength;
                                         memcpy(&buffer[0],
@@ -263,9 +346,9 @@ struct SC64
                                         /* Clean up the packet now that we're
                                          * done using it. */
                                         enet_packet_destroy(enetEvent.packet);
-                                        while (rxready)
-                                        {
-                                        }
+                                        // while (rxready)
+                                        // {
+                                        // }
                                     }
                                     break;
                                     default:
@@ -279,9 +362,22 @@ struct SC64
                 case NETTYPE_UDP_SEND:
                 {
                     ENetPacket *packet = enet_packet_create(
-                        tmp, USBHEADER_GETSIZE(hdr), ENET_PACKET_FLAG_RELIABLE);
+                        tmp, len, ENET_PACKET_FLAG_RELIABLE);
+                    if (!enetHost) {
+                        return;
+                    }
+                    if (!enetPeer) {
+                        enet_host_broadcast(enetHost, 0, packet);
+                        // assume we're the server
+                        enet_host_flush(enetHost);
+                        return;
+                    }
                     enet_peer_send(enetPeer, 0, packet);
-                    enet_host_flush(enetClient);
+                    enet_host_flush(enetHost);
+                    break;
+                }
+                case NETTYPE_TEXT: {
+                    printf("network_printf: %s\n", (const char*)&tmp[0]);
                     break;
                 }
                 default:
@@ -298,10 +394,14 @@ struct SC64
             break;
         }
         case SC64_CMD_DEBUG_RX_DATA:
+        case SC64_CMD_USB_READ:
         {
-            addr = SC64_SDRAM_BASE + (data[0]);
-            len = (data[1]);
+            if (cmd == SC64_CMD_USB_READ) {
 
+            addr = (data[0]);
+            } else addr = data[0] + SC64_SDRAM_BASE;
+            len = USBHEADER_GETSIZE(data[1]);
+            //printf("reading %d bytes from %08X\n", len, addr);
             rxbusy = true;
             for (i = addr; i < addr + len; i++)
             {
